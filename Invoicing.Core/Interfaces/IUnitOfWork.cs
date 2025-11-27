2962be62098ef7ef7328aedff23a6815fb1395d1
Lucas Curual <lucascurual97@gmail.com>
2025-11-26 23:34:07 -0300

    Se añade la interfaz IUnitOfWork para transaccionar en la BD

---
+++ Invoicing.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Invoicing.Core.Interfaces;
+
+public interface IUnitOfWork
+{
+    Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+
+    Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+    
+    Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
+
+    Task SaveChangesAsync(CancellationToken cancellationToken = default);
+}
