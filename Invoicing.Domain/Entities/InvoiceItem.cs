f25bc3d95c4cec228b880b59a24792a9e44188ad
Lucas Curual <lucascurual97@gmail.com>
2025-12-07 19:26:58 -0300

    refactor: Se utilizan excepciones más específicas. Se agrega el campo LineNumber para poder darle un orden específico a los ítems del comprobante.

--- Invoicing.Domain/Entities/InvoiceItem.cs
+++ Invoicing.Domain/Entities/InvoiceItem.cs
@@ -7,25 +7,36 @@ public class InvoiceItem : TenantEntity
   public Guid Id { get; private set; }
   public Guid InvoiceId { get; private set; }
   public Guid ProductId { get; private set; }
+  public int LineNumber { get; private set; }
   public decimal Quantity { get; private set; }
   public decimal UnitPrice { get; private set; }
   public string Description { get; private set; }
   public decimal Subtotal { get; private set; }
   public decimal TaxAmount { get; private set; }
 
-  public InvoiceItem(Guid invoiceId, Guid tenantId, Guid productId, string description, decimal quantity, decimal unitPrice, decimal taxAmount) : base(tenantId)
+  public InvoiceItem(Guid invoiceId, Guid tenantId, int lineNumber, Guid productId, string description, decimal quantity, decimal unitPrice, decimal taxAmount) : base(tenantId)
   {
     if (invoiceId == Guid.Empty) throw new ArgumentException("InvoiceId no puede estar vacío.", nameof(invoiceId));
     if (productId == Guid.Empty) throw new ArgumentException("ProductId no puede estar vacío.", nameof(productId));
 
-    if (quantity <= 0) throw new ArgumentException("Quantity debe ser un numero mayor a cero.", nameof(quantity));
-    if (unitPrice <= 0) throw new ArgumentException("UnitPrice debe ser un numero mayor a cero.", nameof(unitPrice));
-    if (taxAmount < 0) throw new ArgumentException("TaxAmount debe ser un numero positivo.", nameof(unitPrice));
+    if (lineNumber <= 0)
+      throw new ArgumentOutOfRangeException(nameof(lineNumber), lineNumber,
+        $"LineNumber debe ser mayor a cero. Valor recibido {lineNumber}");
+    if (quantity <= 0)
+      throw new ArgumentOutOfRangeException(nameof(quantity), quantity, 
+        $"Quantity debe ser mayor a cero. Valor recibido: {quantity}");
+    if (unitPrice <= 0) 
+      throw new ArgumentOutOfRangeException(nameof(unitPrice), unitPrice,
+        $"UnitPrice debe ser mayor a cero. Valor recibido {unitPrice}");
+    if (taxAmount < 0)
+      throw new ArgumentOutOfRangeException(nameof(taxAmount), taxAmount,
+        $"TaxAmount debe ser mayor a cero. Valor recibido {taxAmount}");
 
     if (string.IsNullOrWhiteSpace(description)) throw new ArgumentException("Description no puede estar vacío.", nameof(description));
 
     Id = Guid.NewGuid();
     InvoiceId = invoiceId;
+    LineNumber = lineNumber;
     ProductId = productId;
     Quantity = quantity;
     UnitPrice = unitPrice;
