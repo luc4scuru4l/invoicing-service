540f6ec1aeac16392a6b0c3465c57beaff4bc337
Lucas Curual <lucascurual97@gmail.com>
2025-12-07 22:41:37 -0300

    Se crea el mapeo de PointOfSaleCounter

---
+++ Invoicing.Infrastructure/Persistence/Configurations/PointOfSaleCounterConfiguration.cs
@@ -0,0 +1,22 @@
+using Invoicing.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Invoicing.Infrastructure.Persistence.Configurations;
+
+public class PointOfSaleCounterConfiguration : IEntityTypeConfiguration<PointOfSaleCounter>
+{
+  public void Configure(EntityTypeBuilder<PointOfSaleCounter> builder)
+  {
+    builder.ToTable("PointOfSaleCounters");
+
+    builder.HasKey(x => new { x.PointOfSaleId, x.ArcaVoucherCode });
+
+    builder.Property(x => x.LastNumber).IsConcurrencyToken();
+
+    builder.HasOne<PointOfSale>()
+           .WithMany()
+           .HasForeignKey(x => x.PointOfSaleId)
+           .OnDelete(DeleteBehavior.Restrict);
+  }
+}
