1bb6b1a6086ee708d35efbb93c87e5af7a449717
Lucas Curual <lucascurual97@gmail.com>
2025-12-07 21:04:05 -0300

    refactor: Se agrega constructor público

--- Invoicing.Domain/Entities/Tax.cs
+++ Invoicing.Domain/Entities/Tax.cs
@@ -7,6 +7,17 @@ public class Tax : TenantEntity
   public Guid Id { get; private set; }
   public string Name { get; private set; } // "IVA 21%"
   public decimal Rate { get; private set; } // 0.21
-  
+
+  public Tax(Guid tenantId, string name, decimal rate) : base(tenantId)
+  {
+    if (rate < 0 || rate > 1)
+      throw new ArgumentOutOfRangeException(nameof(rate), rate,
+        $"Rate debe ser mayor a cero y menor a 1. Valor recibido {rate}");
+
+    Id = Guid.NewGuid();
+    Name = name;
+    Rate = rate;
+  }
+
   private Tax() { Name = null!; }
 }
