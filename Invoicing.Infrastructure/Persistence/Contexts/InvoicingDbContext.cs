0cf3dd932a4683493024c1ff587391146a4590e2
Lucas Curual <lucascurual97@gmail.com>
2025-12-09 18:43:48 -0300

    Se agregan las nuevas entidades al context

--- Invoicing.Infrastructure/Persistence/Contexts/InvoicingDbContext.cs
+++ Invoicing.Infrastructure/Persistence/Contexts/InvoicingDbContext.cs
@@ -12,8 +12,17 @@ public class InvoicingDbContext : DbContext
 
   public DbSet<Invoice> Invoices { get; set; }
   public DbSet<InvoiceItem> InvoiceItems { get; set; }
+  public DbSet<InvoiceItemTax> InvoiceItemTaxes { get; set; }
+
+  public DbSet<PointOfSale> PointOfSales { get; set; }
   public DbSet<PointOfSaleCounter> PointOfSaleCounters { get; set; }
 
+  public DbSet<AuthorizationRequest> AuthorizationRequests { get; set; }
+
+  public DbSet<Tax> Taxes { get; set; }
+  public DbSet<TaxRule> TaxRules { get; set; }
+
+  public DbSet<Tenant> Tenants { get; set; }
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
     base.OnModelCreating(modelBuilder);
