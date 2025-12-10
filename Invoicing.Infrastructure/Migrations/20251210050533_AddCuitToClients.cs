e203dccefd89057bc1bb33a07b685d2dfd4cb4d1
Lucas Curual <lucascurual97@gmail.com>
2025-12-10 02:11:05 -0300

    fix: Se añade el campo Cuit de Clients a la migración

---
+++ Invoicing.Infrastructure/Migrations/20251210050533_AddCuitToClients.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Invoicing.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCuitToClients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Cuit",
+                table: "Clients",
+                type: "nchar(11)",
+                fixedLength: true,
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Cuit",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nchar(11)",
+                oldFixedLength: true,
+                oldMaxLength: 11);
+        }
+    }
+}
